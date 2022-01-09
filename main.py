bafb5d1eadde0da254eb1415016fe8c52628511f
MohammedHMateen <myfriendhammad@gmail.com>
2022-01-09 23:35:56 +0530

    added Query params

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.params import Query
+
 from model.user import *
 
 test_app = FastAPI()
@@ -38,7 +40,30 @@ def get_users_by_id(uid: int):
     for user in list_db:
         if user.id == uid:
             return user
-    return list_db
+    raise HTTPException(
+        status_code=404,
+        detail=f"Can't find any user with id: {uid}"
+    )
+
+
+# With query parameters /api/users?uid=1
+# Query Params with extra informations
+# q2: bool = Query(True, description = "Query with extra bool info")
+# q1: int = Query(..., ge=0),
+@test_app.get("/api/users/")
+def get_users_by_id_in_query(uid: int,
+                             q1: int = Query(any, ge=0),
+                             q2: bool = Query(True, description="Query with extra bool info")):
+    listy = []
+    for user in list_db:
+        if user.id == uid:
+            listy.append(user)
+            listy.append(f"{q1}&&{q2}")
+            return listy
+    raise HTTPException(
+        status_code=404,
+        detail=f"Can't find any user with id: {uid}"
+    )
 
 
 @test_app.post("/api/users")
